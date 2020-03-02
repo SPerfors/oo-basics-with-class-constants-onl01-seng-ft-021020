fff6eef241501af896ee54797daf2d9898d11d75
Steven Perfors <stevecanbuild@gmail.com>
2020-03-02 20:18:05 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -6,6 +6,7 @@ class Shoe
 
   def initialize(brand)
     @brand = brand
+    BRAND << brand 
   end
 
   def cobble
@@ -13,8 +14,8 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand
-    BRAND << brand 
-  end
+  # def brand
+  #   BRAND << brand 
+  # end
 
 end
